07a97562b5cc52f1258395afbc8d83813701aa4b
sheikhrasel35 <abushaidrasel@gmail.com>
2025-08-31 00:32:53 +0600

    add the chain.js

---
+++ chain.js
@@ -0,0 +1,16 @@
+const student = {
+    name: 'rasel',
+    1: 50,
+    family: {
+        title: 'sheikh',
+        // mother: {
+        //     name: 'Beauti',
+        //     age: 45
+        // }
+    },
+   'home-address'gaibandha',
+    marks: 90
+}
+
+// console.log(student.family.title)
+console.log(student.family.mother?.age)
